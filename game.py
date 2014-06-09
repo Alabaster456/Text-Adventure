ab3b282d38c37ac7a00d1088f30fffbd872acc55
Yazid <Alabaster456@users.noreply.github.com>
2014-06-09 07:32:50 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,21 @@
-""" This is where the game is going to be at in whole! """
+#VARIABLES 
+"""This is just a startup. The list will get bigger!"""
+userHP = 50 #user's health
+dead = False #Checks to see if the user is alive or not.
+inventory = ["", "", ""] #Items the user can hold (could expand the list)
 
-user = raw_input("What's your name?")
+
+print("-----------------------------")
+print("|| Welcome to Pyth-Adventure!  ||") 
+print("-----------------------------")
+
+
+print ("-------               ---------") 
+print("|      |             |          |")
+print("| Play |       OR    | Tutorial |") 
+print(" ------               ----------")
+question = raw_input("If you want to begin playing, type play. If you want to read about the tutorial, type tutorial!").upper()
+if question == "PLAY":
+    print("Okay! Let's begin!")
+elif question == "TUTORIAL":
+    print("GOOD!")
